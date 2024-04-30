7c2af4f8880591134ec7803cd535677fa98eaf05
Kapinge De Almeida <kalmeida-admin@quality.co.ao>
2024-04-30 19:11:54 +0100

    Início

---
+++ server.js
@@ -0,0 +1,6 @@
+const port = 3000
+
+// Escutar a porta 3000
+app.listen(port, () => {
+    console.log(`Servidor rodando no endereço hppt://localhost:${port}`)
+})
---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+
+// criar rota padrão ou raiz
+
+app.get('/', (req, res) => {
+    const x = {
+        "name": 'Kapinge Almeida',
+        "idade": 20,
+        "email": 'kapinge@almeida.com',
+        "endereco": {
+            "rua": '16',
+            "bairro": 'Zona verde 3',
+            "cidade": 'Luanda'
+        }
+    }
+    res.send(x)
+})
+
+
